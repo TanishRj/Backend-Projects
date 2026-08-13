5d307957a4f569b55e30440921afe5cb5e7e79f7
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-08-13 22:49:15 +0530

    u

--- 1. URL Shortner/utils/hash.js
+++ 1. URL Shortner/utils/hash.js
@@ -1,10 +1,10 @@
 import { randomBytes, createHmac } from "node:crypto"
 
+export function hashPasswordWithSalt(password){
+    // Hashing password using crypto module
+    const salt = randomBytes(256).toString('hex')
+    // Hashing password using generated salt
+    const hashedPassword = createHmac('sha256', salt).update(password).digest('hex')
 
-// Hashing password using crypto module
-
-const salt = randomBytes(256).toString('hex')
-
-// Hashing password using generated salt
-
-const hashedPassword = createHmac('sha256', salt).update(password).digest('hex')
+    return {salt, password: hashedPassword}
+}
