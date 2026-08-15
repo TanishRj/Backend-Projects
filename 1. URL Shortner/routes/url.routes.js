46dbe7650ddb79d5540991c0dd471c31725db3eb
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-08-15 14:05:50 +0530

    u

--- 1. URL Shortner/routes/url.routes.js
+++ 1. URL Shortner/routes/url.routes.js
@@ -1,6 +1,7 @@
 // Importing express
 import express from 'express'
-
+// importing url validation 
+import {} from '../validation/request.validation.js'
 // creating router from express
 const router = express.Router()
 
@@ -8,6 +9,9 @@ const router = express.Router()
 router.post('/shorten', async function (req, res) {
     // Making sure user is authenticated
     const userID = req.user.id
+
+    // Returning response if user id does not exists
+    if (!userID) return res.status(401).json({error: `You must be logged in to access this resource`})
 })
 
 
