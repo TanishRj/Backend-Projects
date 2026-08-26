1b72541db4581053fd7d52a3f2e2a7adaa8d9176
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-08-26 21:15:19 +0530

    Update index.js

--- 1. URL Shortner/index.js
+++ 1. URL Shortner/index.js
@@ -1,26 +1,18 @@
 import express from 'express'
-// Importing authentication middleware
-import {authenticationMiddleware} from './middleware/auth.middleware.js'
-
-// Importing user router
+import { authenticationMiddleware } from './middleware/auth.middleware.js'
 import userRouter from './routes/user.routes.js'
-
-// Importing url Router
 import urlRouter from './routes/url.routes.js'
 
 const app = express()
-const PORT = process.env.PORT ?? 8000
 
 app.use(express.json())
 app.use(authenticationMiddleware)
 
 app.get('/', async (req, res) => {
-    return res.json({status : `API Health OK`})
+    return res.json({ status: 'API Health OK' })
 })
 
 app.use('/user', userRouter)
 app.use(urlRouter)
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+export default app
