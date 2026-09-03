fa96276a1ec5d75034b214aa77faea409421a783
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-03 11:03:39 +0530

    Update app.js

--- base-camp/src/app.js
+++ base-camp/src/app.js
@@ -4,6 +4,12 @@ import express from "express"
 // Creating new app from express
 const app = express()
 
+// Middlewares 
+app.use(express.json({limit: "16kb"})) // Anybody can send json data with limit
+// Accepting URL encoded data
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+// Static asset to be viewed publically
+
 // Creating a GET request which sends some responds if requested 
 app.get('/', (req, res) => {
 // Sending server response as API Health OK
