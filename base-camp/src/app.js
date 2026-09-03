364458af481fea6e748bf6d5352377a5824b068d
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-03 11:03:58 +0530

    Update app.js

--- base-camp/src/app.js
+++ base-camp/src/app.js
@@ -9,7 +9,7 @@ app.use(express.json({limit: "16kb"})) // Anybody can send json data with limit
 // Accepting URL encoded data
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 // Static asset to be viewed publically
-
+app.use(express.static)
 
 // Creating a GET request which sends some responds if requested 
 app.get('/', (req, res) => {
