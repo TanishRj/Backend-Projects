1fefae906f08b13dd1eede6d5c9f4af840307451
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-03 11:14:46 +0530

    Update app.js

--- base-camp/src/app.js
+++ base-camp/src/app.js
@@ -1,8 +1,12 @@
 // Importing express
 import express from "express"
 
+// Import cors
+import cors from "cors"
+
 // Creating new app from express
 const app = express()
+
 
 // Middlewares 
 app.use(express.json({limit: "16kb"})) // Anybody can send json data with limit
@@ -10,6 +14,17 @@ app.use(express.json({limit: "16kb"})) // Anybody can send json data with limit
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 // Static asset to be viewed publically
 app.use(express.static("public"))
+
+// Cors Configuration
+app.use(cors({
+    // Take origin value from env and split it from ',' , else take vite http
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    // Supported Method
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    // Headers config
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
 
 // Creating a GET request which sends some responds if requested 
 app.get('/', (req, res) => {
