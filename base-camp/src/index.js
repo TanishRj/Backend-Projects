cd70aaf4b9e468aa4e537a27008467c643ff53d4
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-04 18:01:33 +0530

    Update index.js

--- base-camp/src/index.js
+++ base-camp/src/index.js
@@ -1,6 +1,9 @@
 import dotenv from "dotenv"
 // Importing app from app.js
 import app from './app.js'
+// Importing connect method
+import connectDB from "./db/index.js"
+
 dotenv.config({
     path: "./.env"
 })
@@ -8,7 +11,14 @@ dotenv.config({
 // Creating port from env, else 3000
 const port = process.env.PORT || 3000
 
-// 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+// First connect to mongodb and then start listening on port for server
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
+})
+  // Catch error and exit process
+  .catch((err) => {
+    
+  })
