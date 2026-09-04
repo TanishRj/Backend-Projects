2daba0392ece431337cbd1e2d483c6bbafecb849
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-04 18:31:14 +0530

    Create healthcheck.controller.js

---
+++ base-camp/src/controllers/healthcheck.controller.js
@@ -0,0 +1,12 @@
+// Importing api response 
+import {ApiResponse} from "../utils/api-response.js"
+
+// Creating a new health check method
+const healthCheck = (req, res) => {
+    // Wrapping into try catch
+    try {
+        res.status(200).json()
+    } catch (error) {
+        
+    }
+}
