da52708aee6aab97d6c6c6dfeba45c6e0467fbbc
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-04 17:25:40 +0530

    Create index.js

---
+++ base-camp/src/db/index.js
@@ -0,0 +1,15 @@
+// Importing Mongoose
+import mongoose from "mongoose";
+
+// Creating a async method for db connection
+const connectDB = async () => {
+    try {
+        
+    } catch (error) {
+        // Log if any error occurs
+        
+    }
+}
+
+// Exporting method
+export default connectDB
