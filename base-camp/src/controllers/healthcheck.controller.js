d9684d52c37cf5fd567c47577eeb22029ac48f00
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-04 19:29:59 +0530

    Update healthcheck.controller.js

--- base-camp/src/controllers/healthcheck.controller.js
+++ base-camp/src/controllers/healthcheck.controller.js
@@ -6,9 +6,13 @@ const healthCheck = (req, res) => {
     // Wrapping into try catch
     try {
         res.status(200).json(
-            
+            // Create a new object of our api response class
+            new ApiResponse(200, {message : "Server is Running"})
         )
     } catch (error) {
         
     }
 }
+
+// Exporting Health Check
+export { healthCheck }
