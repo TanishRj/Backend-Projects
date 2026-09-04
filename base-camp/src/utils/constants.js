1f25f271e8c996c970bc8c105ebab0d925d2c418
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-04 17:06:31 +0530

    Update constants.js

--- base-camp/src/utils/constants.js
+++ base-camp/src/utils/constants.js
@@ -1,4 +1,4 @@
-// Creating and exporting User roles 
+// Creating and exporting User roles object
 export const UserRolesEnum = {
     ADMIN: "admin",
     PROJECT_ADMIN: "project_admin",
@@ -7,3 +7,13 @@ export const UserRolesEnum = {
 
 // Exporting roles available as array so that it can be looped through 
 export const AvailableUserRole = Object.values(UserRolesEnum)
+
+// Creating and exporting Task status object
+export const TaskStatusEnum = {
+    TODO: "todo",
+    IN_PROGRESS: "in_progress",
+    DONE: "done"
+}
+
+// Exporting task status available as array so that it can be looped through 
+export const AvailableTaskStatus = Object.values(TaskStatusEnum)
