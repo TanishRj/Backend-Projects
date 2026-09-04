ffacac0db3884cc6bd924c3e4dc7435380a2e8e6
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-04 17:43:46 +0530

    Update index.js

--- base-camp/src/db/index.js
+++ base-camp/src/db/index.js
@@ -4,10 +4,15 @@ import mongoose from "mongoose";
 // Creating a async method for db connection
 const connectDB = async () => {
     try {
-        
+        // Connect mongodb using mongoose
+        await mongoose.connect(process.env.MONGO_URI)
+        // Log if connected
+        console.log("MongoDB Connected ✅");
     } catch (error) {
         // Log if any error occurs
-        await mongoose.connect(process.env.MONGO_URI)
+        console.error("  MongoDB Connection error ❌ ", error)
+        // Exit process if any error occurs
+        process.exit(1)
     }
 }
 
