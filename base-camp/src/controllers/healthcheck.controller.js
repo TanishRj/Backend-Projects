ed4eb6f6abf4ddd06b5f4b191b805dca838a4fd8
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-05 13:21:33 +0530

    Update healthcheck.controller.js

--- base-camp/src/controllers/healthcheck.controller.js
+++ base-camp/src/controllers/healthcheck.controller.js
@@ -3,22 +3,11 @@ import {ApiResponse} from "../utils/api-response.js"
 // Importing Async Handler
 import { asyncHandler } from "../utils/async-handler.js"
 
-// Creating a new health check method
-// const healthCheck = (req, res) => {
-//     // Wrapping into try catch
-//     try {
-//         res.status(200).json(
-//             // Create a new object of our api response class
-//             new ApiResponse(200, {message : "Server is Running"})
-//         )
-//     } catch (error) {
-        
-//     }
-// }
-
 // Creating new healthcheck method with asynchandler
 const healthCheck = asyncHandler(async (req, res) => {
-
+    res.status(200).json(
+        new ApiResponse(200, {message: "Server is Running"})
+    )
 })
 
 // Exporting Health Check
