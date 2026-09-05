c638918a6d234dd835bc86da387f86efcf54a4b8
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-05 14:30:38 +0530

    Update user.models.js

--- base-camp/src/models/user.models.js
+++ base-camp/src/models/user.models.js
@@ -1,4 +1,8 @@
 // Importing mongoose and schema
 import mongoose, { Schema } from "mongoose";
 
+// Creating new userschema using Schema object
 const userSchema = new Schema({})
+
+// Exporting user schema to model so we can use it
+export const User = mongoose.model("User", userSchema)
