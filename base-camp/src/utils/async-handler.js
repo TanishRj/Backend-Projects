456232e92a08084a1c1b6c4c49340da590b748e3
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-05 13:12:02 +0530

    u

---
+++ base-camp/src/utils/async-handler.js
@@ -0,0 +1,12 @@
+// Creating an async handler function accepts another function in requestHandler
+// It is also known as higher order function which means whole function is passed
+const asyncHandler = (requestHandler) => {
+    // req, res and next is returned by express 
+    return (req, res, next) => {
+        // Resolve the function if possible using promise
+        Promise
+        .resolve(requestHandler(req, res, next))
+        // Catch error and pass the error to express error handler
+        .catch((err) => next(err))
+    }
+}
