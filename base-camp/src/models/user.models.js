a78fd88de047361d9bfa0ffad3eb676a7d9c4411
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-05 14:32:52 +0530

    Update user.models.js

--- base-camp/src/models/user.models.js
+++ base-camp/src/models/user.models.js
@@ -2,7 +2,18 @@
 import mongoose, { Schema } from "mongoose";
 
 // Creating new userschema using Schema object to define fields
-const userSchema = new Schema({})
+const userSchema = new Schema({
+    avatar: {
+        type: {
+            url: String,
+            localPath: String
+        },
+        default: {
+            url: ``,
+            localPath: ""
+        }
+    }
+})
 
 // Exporting user schema to model so we can use it
 export const User = mongoose.model("User", userSchema)
