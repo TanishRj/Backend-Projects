6381bb34fe4b4eb5dd71a51eec94ccedd914868c
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-07 10:30:12 +0530

    Update user.models.js

--- base-camp/src/models/user.models.js
+++ base-camp/src/models/user.models.js
@@ -102,7 +102,9 @@ userSchema.methods.isPasswordCorrect = async function (password) {
 }
 
 // Method to generate access token
-userSchema.methods.generateAccessToken = 
+userSchema.methods.generateAccessToken = function () {
+    
+}
 
 // Exporting user schema to model so we can use it
 export const User = mongoose.model("User", userSchema)
