8a5d4f0b436bb9771d8c1d9041c39e239c0bfc13
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-07 11:42:54 +0530

    u

---
+++ base-camp/src/utils/mail.js
@@ -0,0 +1,46 @@
+// importing mailgen
+import Mailgen from "mailgen";
+
+// Creating email verification email template which will take username and verification url
+const emailVerificationMailgenContent = (username, verificationUrl) => {
+    return {
+        body: {
+            name: username,
+            intro: "Welcome to Tanish's App, Let's get started",
+            action: {
+                instructions: "Verify your email by click the following button",
+                button: {
+                    color: "#015eea",
+                    text: "Verify your email",
+                    link: verificationUrl
+                },
+            },
+            outro: "Have questions? Reply to this email"
+        }
+    }
+}
+
+// Creating forgot password email template which will take username and verification url
+const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
+    return {
+        body: {
+            name: username,
+            intro: "We got a request to reset the password of your account",
+            action: {
+                instructions: "To reset the password, click on the followig button or link",
+                button: {
+                    color: "#015eea",
+                    text: "Reset Password",
+                    link: passwordResetUrl
+                },
+            },
+            outro: "Have questions? Reply to this email"
+        }
+    }
+}
+
+// Exporting both email templates
+export {
+    emailVerificationMailgenContent,
+    forgotPasswordMailgenContent
+}
