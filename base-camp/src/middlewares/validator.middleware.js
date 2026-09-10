a6365edcd6a5798d320d150ddebc1832d54a3486
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-10 11:43:11 +0530

    Update validator.middleware.js

--- base-camp/src/middlewares/validator.middleware.js
+++ base-camp/src/middlewares/validator.middleware.js
@@ -3,3 +3,10 @@ import {validationResult} from "express-validator"
 // Importing api error class
 import { ApiError } from "../utils/api-error.js"
 
+// Writing validate method to validate request
+export const validate = (req, res, next) => {
+    const errors = validationResult(req)
+    if(errors.isEmpty()){
+        return next()
+    }
+}
