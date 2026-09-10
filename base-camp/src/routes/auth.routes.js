7dd7181160dff630eb8aea7b7cd6c16131ec81e8
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-10 12:24:44 +0530

    u

--- base-camp/src/routes/auth.routes.js
+++ base-camp/src/routes/auth.routes.js
@@ -4,11 +4,20 @@ import { Router } from "express";
 // Importing register user controller
 import {registerUser} from "../controllers/auth.controllers.js"
 
+// Importing validate middleware
+import {validate} from  "../middlewares/validator.middleware.js"
+
+// Importing validation
+import {userRegisterValidator} from "../validators/index.js"
+
 // Creating a router
 const router = Router()
 
 // Creating post route to register user
-router.route("/register").post(registerUser)
+// 1. Run validation and collect some errors
+// 2. Give errors to validate middleware to handle them
+// 3. Then pass the errors if present else go to next() i.e. registerUser Route
+router.route("/register").post(userRegisterValidator(), validate, registerUser)
 
 // Exporting Router
 export default router
--- base-camp/src/validators/index.js
+++ base-camp/src/validators/index.js
@@ -1,2 +1,52 @@
 // Importing body from express to fetch data from body of request 
+import {body} from "express-validator"
 
+// Creating new method for user register validation 
+const userRegisterValidator = () => {
+    return [
+        // Email validation
+        body("email")
+            // Trim email
+            .trim() 
+
+            // Check if email is not empty and return message
+            .notEmpty()
+            .withMessage("Email is required")
+            
+            // Checking if email is a proper email
+            .isEmail()
+            .withMessage("Email is invalid"),
+            
+            // Username Validation
+            body("username")
+            // Trim username
+            .trim()
+
+            // should not be empty with message
+            .notEmpty()
+            .withMessage("Usernam is required")
+            
+            // username should be lowercase with message
+            .isLowercase()
+            .withMessage("Usernam must be in lowercase")
+            
+            // Should be minimum of 3 length
+            .isLength({min: 3})
+            .withMessage("Username must be must be at least 3 characters"),
+
+        // Password field validation
+        body("password")
+            .trim()
+            .notEmpty()
+            .withMessage("Password should not be empty"),
+        
+        // Full Name validation
+        body("fullName")
+            .optional().trim() 
+        ]       
+}
+
+// Exporting method
+export {
+    userRegisterValidator
+}
