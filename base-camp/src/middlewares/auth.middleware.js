4722681f6f533c59242f00c1079e34cc272848fb
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-12 14:57:49 +0530

    Update auth.middleware.js

--- base-camp/src/middlewares/auth.middleware.js
+++ base-camp/src/middlewares/auth.middleware.js
@@ -4,6 +4,8 @@ import {User} from "../models/user.models.js"
 import {ApiError} from "../utils/api-error.js"
 // Importing async error handler class 
 import {asyncHandler} from "../utils/async-handler.js"
+// Importing jsonwebtoken
+import jwt from 'jsonwebtoken'
 
 // Creating method to verify JWT
 export const verifyJWT = asyncHandler(async(req, res, next) => {
@@ -11,8 +13,22 @@ export const verifyJWT = asyncHandler(async(req, res, next) => {
     // Also replacing the Bearer <token> to only <token> by replace method
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
 
-    
+    // Throwing error if no token recieved 
     if(!token){
         throw new ApiError(401, "Unauthorized Request")
+    }
+
+    // Verifying the token under try catch (try catch for extra safety)
+    try {
+        // Decoding token 
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        
+        // Getting user based on _id from the decoded token
+        // Selecting only required field
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken -emailVerificationToken -emailVerificationExpiry")
+
+        
+    } catch (error) {
+        
     }
 })
