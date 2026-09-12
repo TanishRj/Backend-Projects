a82528b508045db29fba65e528bee493aff50782
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-12 14:50:29 +0530

    Update auth.middleware.js

--- base-camp/src/middlewares/auth.middleware.js
+++ base-camp/src/middlewares/auth.middleware.js
@@ -7,5 +7,12 @@ import {asyncHandler} from "../utils/async-handler.js"
 
 // Creating method to verify JWT
 export const verifyJWT = asyncHandler(async(req, res, next) => {
+    // Fetching encrypted accessToken from cookies or authorization header
+    // Also replacing the Bearer <token> to only <token> by replace method
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
     
+    if(!token){
+        throw new ApiError(401, "Unauthorized Request")
+    }
 })
