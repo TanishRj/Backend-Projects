5ae8eb435c91890551100f0adff7c9a975c8ad60
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-17 14:46:55 +0530

    Update index.js

--- base-camp/src/validators/index.js
+++ base-camp/src/validators/index.js
@@ -107,5 +107,6 @@ export {
     userRegisterValidator,
     userLoginValidator,
     userChangeCurrentPasswordValidator,
-    userForgotPasswordValidator
+    userForgotPasswordValidator,
+    userResetForgotPasswordValidator
 }
