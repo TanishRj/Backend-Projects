a5de69eda58117f29ff14e264f599cedab898b13
Tanish Choudhary <92745452+TanishRj@users.noreply.github.com>
2026-09-21 16:19:48 +0530

    Update app.js

--- base-camp/src/app.js
+++ base-camp/src/app.js
@@ -46,7 +46,7 @@ app.get('/', (req, res) => {
   res.json({
         Project: ' 🏡 Basecamp 🏡',
         status: 'API Health OK ✅',
-        Madeby: '❄️ Tanish Choudhary ❄️',
+        Madeby: '❄️  ❄️',
         Github: '🌀 https://github.com/TanishRj 🌀',
     })
 })
